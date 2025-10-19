ee08935a9bcfef1c1dc4552369c201f74d5b6f06
Arslan Aslam <shaniarslanali123@gmail.com>
2025-10-20 02:46:41 +0500

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-
+import { Bookmark } from 'lucide-react'
 const Card = (props) => {
 
     return (
         <div className="card">
-            <img src={props.img} alt="" />
-            <h1>{props.user},{props.age}</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <button>View Profile</button>
+            <div>
+                <div className="top">
+                    <img src={props.brandLogo} alt="nachioooooo" />
+                    <button>Save <Bookmark size={10} /> </button>
+                </div>
+                <div className="center">
+                    <h3>{props.company} <span>{props.datePosted}</span></h3>
+                    <h2>{props.post}</h2>
+                    <div className='tag'>
+                        <h4>{props.tag1}</h4>
+                        <h4>{props.tag2}</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div>
+                    <h3>{props.pay}</h3>
+                    <p>Mumbai, India</p>
+                </div>
+                <button>Apply Now</button>
+            </div>
         </div>
     )
 }
